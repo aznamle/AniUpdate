1f6b7f3e8bba69078ad75967fcda42de4c8f8bd8
aznamle <namle1998@hotmail.com>
2021-05-25 10:38:00 -0700

    switched to implicit grant for token

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Link from 'next/link'
 import { getToken } from './login'
 import { useRouter } from 'next/router'
+import Test from "./Test";
 
 
 export default function Home() {
@@ -9,39 +10,16 @@ export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    
+    const token = getToken()
+
+    if(token) {
+      setToken(token)
     }
   }, [])
 
-  const client_id = '2467'
-  const redirect_uri = 'http://localhost:3000'
-  const client_secret = 'dA0WXcLbo4mD4tob3vXskQAjP2SwvbVKgA7KqK3Q'
-
-  const PostToken = () => {
-    const code = getToken()
-
-    const data = fetch('https://anilist.co/api/v2/oauth/token', {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            'grant_type': 'authorization_code',
-            'client_id': `${client_id}`,
-            'client_secret': `${client_secret}`,
-            'redirect_uri': `${redirect_uri}`, // http://example.com/callback
-            'code': `${code}`, // The Authorization Code received previously
-          })
-    })
-
-    console.log(data)
-  }
-
   return (
     <div className="flex justify-center items-center text-center">
-      { token ? <></> :
+      { token ? <Test />:
       <div>
       <h1>login please</h1>
       <Link href='/login'>
--- pages/login.js
+++ pages/login.js
@@ -6,15 +6,15 @@ const redirect_uri = 'http://localhost:3000'
 const client_secret = 'dA0WXcLbo4mD4tob3vXskQAjP2SwvbVKgA7KqK3Q'
 
 export const getToken = () => {
-    const token = window.location.search.split('=')
+    const token = window.location.hash.split('=')
     return token[1]
 }
 
 const Login = () => {    
 
       return (
-        <div className='container mx-auto text-center items-center justify-center'>
-          <a href={`https://anilist.co/api/v2/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`}>Login with AniList</a>    
+        <div className='flex container mx-auto text-center items-center justify-center'>
+          <a href={`https://anilist.co/api/v2/oauth/authorize?client_id=${client_id}&response_type=token`}>Login with AniList</a>    
         </div>
       )
 }
